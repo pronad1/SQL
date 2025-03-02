1bfe16c4b7ce5a2e603e0517b5291765c0ed0415
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-02 22:39:30 +0600

    3/2/25

--- Chapter_30_Trigger.sql
+++ Chapter_30_Trigger.sql
@@ -216,13 +216,38 @@ insert into edge values
 select * from edge;
 
 CREATE table edge_audit(
-    Title varchar2(100) primary key,
+    Title varchar2(100),
     Publisher VARCHAR2(20),
     CategoryName varchar2(20),
     Old_Rating NUMERIC,
     New_Rating NUMERIC,
     Audit_Date DATE
 );
-
+-- DROP TABLE EDGE_AUDIT;
+-- DROP TRIGGER edge_audit_rec;
 SELECT * from EDGE_AUDIT;
 
+create or replace trigger edge_audit_rec
+before insert or update of rating on edge
+for each row BEGIN
+if inserting THEN
+insert into EDGE_AUDIT
+(Title,PUBLISHER,CategoryName,NEW_RATING,AUDIT_DATE)
+VALUES
+(:new.Title,:new.Publisher,:new.CategoryName,:new.rating,SYSDATE);
+ELSE
+INSERT into EDGE_AUDIT
+(Title,PUBLISHER,CATEGORYNAME,OLD_RATING,NEW_RATING,AUDIT_DATE)
+VALUES
+(:old.title,:old.Publisher,:old.CategoryName,:old.rating,:new.rating,SYSDATE);
+end IF;
+end;
+/
+
+INSERT into EDGE
+values('SHOWLESS','MARING','AUDITER',10);
+
+SELECT * from EDGE_AUDIT;
+select * from EDGE;
+
+update EDGE set RATING = 20 where TITLE='SHOWLESS';
