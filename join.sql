0483b2e755f8db5d0f1d2a5e86e7339a952f7324
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-03 21:59:30 +0600

    3/3/25

--- join.sql
+++ join.sql
@@ -20,3 +20,64 @@ SELECT e.emp_id, e.emp_name, s.salary
 FROM Employees e
 LEFT JOIN Salaries s ON e.emp_id = s.emp_id;
 
+-- Right join
+SELECT e.emp_id, e.emp_name, s.salary
+FROM Employees e
+RIGHT JOIN Salaries s ON e.emp_id = s.emp_id;
+
+
+-- Full join
+
+SELECT e.emp_id, e.emp_name, s.salary
+FROM Employees e
+LEFT JOIN Salaries s ON e.emp_id = s.emp_id
+
+UNION
+
+SELECT e.emp_id, e.emp_name, s.salary
+FROM Employees e
+RIGHT JOIN Salaries s ON e.emp_id = s.emp_id;
+
+
+-- Cross join
+SELECT e.emp_id, e.emp_name, s.salary
+FROM Employees e
+CROSS JOIN Salaries s
+WHERE e.emp_id = s.emp_id;
+
+--Creating a Table with Duplicates
+CREATE TABLE EmployeesDuplicate (
+    emp_id INT,
+    emp_name VARCHAR(50)
+);
+
+-- -- DELETE Using ROW_NUMBER()
+-- with cte as (
+--     SELECT * , row_number() over (partition by emp_id order by emp_id) as rn
+--     from EmployeesDuplicate
+-- )
+-- DELETE FROM EmployeesDuplicate
+-- WHERE emp_id IN (
+--     SELECT emp_id
+--     FROM (
+--         SELECT emp_id, row_number() over (partition by emp_id order by emp_id) as rn
+--         FROM EmployeesDuplicate
+--     ) cte
+--     WHERE rn > 1
+-- );
+
+
+-- Deleting duplicate rows in SQL
+DELETE FROM Employees
+WHERE emp_id NOT IN (
+    SELECT MIN(emp_id)
+    FROM Employees
+    GROUP BY emp_name
+);
+
+--DELETE Using DISTINCT and Temporary Table
+CREATE TABLE Temp_Employees AS 
+SELECT DISTINCT * FROM EmployeesDuplicate;
+
+DROP TABLE EmployeesDuplicate;
+ALTER TABLE Temp_Employees RENAME TO EmployeesDuplicate;
