16011752687d318245e73a84a30dd14564eb05c9
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-12 16:31:10 +0600

    3/12/25

--- ch_exercise_03.sql
+++ ch_exercise_03.sql
@@ -12,3 +12,21 @@ takes.sec_id=teaches.sec_id and
 takes.semester=teaches.semester and 
 takes.year=teaches.year and 
 teaches.id=(SELECT ID from instructor where name='Bawa');
+
+--c.Find the highest salary of any instructor.
+SELECT salary from instructor order by salary DESC limit 1;
+
+--d.Find all instructors earning the highest salary (there may be more than one with the same salary).
+SELECT ID,name from instructor
+WHERE salary=(select salary
+from instructor
+order by salary desc
+limit 1);
+
+--e.. Find the enrollment of each section that was offered in Fall 2017.
+SELECT course_id,sec_id,COUNT(*)
+from takes WHERE
+year=2017 and semester='Fall'
+GROUP by course_id,sec_id;
+
+--f.Find the maximum enrollment, a ross all se tions, in Fall 2017.
